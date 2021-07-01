10b0c6eede178d0ba4f827f834bf58e762134b32
michealabaho <michealabaho265@gmail.com>
2021-07-01 21:08:59 +0100

    training, evaluation and mask filling

--- outcome_mask.py
+++ outcome_mask.py
@@ -24,8 +24,10 @@ from transformers import (
     MODEL_FOR_MASKED_LM_MAPPING,
     AutoConfig,
     AutoModelForMaskedLM,
+AutoModelWithLMHead,
     AutoTokenizer,
     DataCollatorForLanguageModeling,
+    DataCollatorForWholeWordMask,
     PreTrainedTokenizerFast,
     HfArgumentParser,
     Trainer,
@@ -57,14 +59,20 @@ def tokenize(input_text, tokenizer):
     tokenized_encodings['labels'] = tokenized_encodings.input_ids.copy()
     return tokenized_encodings
 
-def train(model, train_data, train_args, tokenizer, trainer_API=False):
+def train(model, train_data, eval_data, train_args, tokenizer, trainer_API=False):
     #use the hugging face trainer API
     if trainer_API:
+        data_collator = DataCollatorForWholeWordMask(
+            tokenizer=tokenizer,
+            mlm_probability=0.15,
+            pad_to_multiple_of=None,
+        )
         trainer = Trainer(
             model=model,
             args=train_args,
-            train_dataset=train_data if train_args.do_train else None,
-            # eval_dataset=eval_dataset if training_args.do_eval else None,
+            train_dataset=train_data,
+            eval_dataset=eval_data,
+            data_collator=data_collator,
             tokenizer=tokenizer,
         )
         train_result = trainer.train()
@@ -75,6 +83,16 @@ def train(model, train_data, train_args, tokenizer, trainer_API=False):
         trainer.save_metrics("train", metrics)
         trainer.save_state()
 
+        if train_args.do_eval:
+            eval_results = trainer.evaluate()
+            eval_results["eval_samples"] = len(eval_data)
+            try:
+                perplexity = math.exp(eval_results["eval_loss"])
+            except OverflowError:
+                perplexity = float("inf")
+            metrics["perplexity"] = perplexity
+            trainer.log_metrics("eval", eval_results)
+            trainer.save_metrics("eval", eval_results)
     else:
         train_loader = DataLoader(train_data, batch_size=args.batch_size, shuffle=args.shuffle_train_data)
         optim = AdamW(model.parameters(), lr=args.learning_rate)
@@ -86,24 +104,35 @@ def train(model, train_data, train_args, tokenizer, trainer_API=False):
                 attention_mask = batch['attention_mask'].to(device)
                 outputs = model(input)
 
-def evaluate(data, labels, model, tokenizer):
+def evaluate(data, labels, train_args, model, tokenizer):
     seqs = []
     labels_copy = list(set([x for y in labels.copy() for x in y.split()]))
     labels_copy.remove('O')
-    for txt, lab in zip(data, labels):
-        masked_sequence = f" ".join([tokenizer.mask_token if j in labels_copy else i for i,j in zip(txt.split(), lab.split())])
-        seqs.append(masked_sequence)
-        input = tokenizer.encode(masked_sequence, return_tensors="pt")
-        mask_token_index = torch.where(input == tokenizer.mask_token_id)[1]
-        logits = model(input)[0]
-        if tokenizer.mask_token in masked_sequence:
-            for mask in mask_token_index:
-                mask = torch.unsqueeze(mask, 0)
-                print(txt)
-                print(masked_sequence)
-                mask_token_logits = logits[0, mask, :]
-                top_5_tokens = torch.topk(mask_token_logits, 5, dim=1).indices[0].tolist()
-                print(masked_sequence.replace(tokenizer.mask_token, tokenizer.decode([top_5_tokens[0]])))
+    with open(train_args.output_dir + '/fact_prediction.txt', 'w') as fact:
+        for txt, lab in zip(data, labels):
+            masked_sequence = f" ".join([tokenizer.mask_token if j in labels_copy else i for i,j in zip(txt.split(), lab.split())])
+            seqs.append(masked_sequence)
+            input = tokenizer.encode(masked_sequence, return_tensors="pt")
+            mask_token_index = torch.where(input == tokenizer.mask_token_id)[1]
+            logits = model(input)[0]
+            if tokenizer.mask_token in masked_sequence:
+                top_3_token_predictions = []
+                print(txt, '\n', masked_sequence, '\n')
+                fact.write(txt + '\n')
+                for mask in mask_token_index:
+                    mask = torch.unsqueeze(mask, 0)
+                    mask_token_logits = logits[0, mask, :]
+                    top_3_tokens = torch.topk(mask_token_logits, 3, dim=1).indices[0].tolist()
+                    top_3_token_predictions.append(top_3_tokens)
+
+                for j,token_ids in enumerate(top_3_token_predictions):
+                    # print(PreTrainedTokenizerFast.convert_ids_to_tokens(tokenizer, ids=token_ids))
+                    masked_sequence = masked_sequence.replace(tokenizer.mask_token, tokenizer.decode([token_ids[0]]), 1)
+                    # print(masked_sequence)
+                    fact.write(str(masked_sequence)+'\n')
+                    fact.write('\n')
+        fact.close()
+
 
 def main(args):
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
@@ -111,25 +140,34 @@ def main(args):
     train_args, = parser.parse_args_into_dataclasses()
     tokenizer = AutoTokenizer.from_pretrained(args.pretrained_model)
     model = AutoModelForMaskedLM.from_pretrained(args.pretrained_model)
+    model.to(device)
 
+    # prepare data
     if train_args.do_train:
-        model.to(device)
         train_data, train_data_labels = prepare_data.read_outcome_data_to_sentences(args.data+'train.txt')
-        train_data = train_data[:20]
         train_tokenized_input = tokenize(train_data, tokenizer)
         train_data = Outcome_Dataset(train_tokenized_input)
-        print(train_data)
-        train(model=model, train_data=train_data, train_args=train_args, tokenizer=tokenizer, trainer_API=True)
 
     if train_args.do_eval:
-        eval_data, eval_data_labels = prepare_data.read_outcome_data_to_sentences(args.data+'dev.txt')
-        evaluate(data=eval_data, labels=eval_data_labels, model=model, tokenizer=tokenizer)
+        eval_data, eval_data_labels = prepare_data.read_outcome_data_to_sentences(args.data + 'dev.txt')
+        eval_tokenized_input = tokenize(eval_data, tokenizer)
+        eval_data = Outcome_Dataset(eval_tokenized_input)
+
+    #training and evaluation
+    if train_args.do_train:
+        train(model=model, train_data=train_data, eval_data=eval_data, train_args=train_args, tokenizer=tokenizer, trainer_API=True)
+
+    if args.do_fill:
+        eval_model = AutoModelForMaskedLM.from_pretrained(train_args.output_dir)
+        eval_data, eval_data_labels = prepare_data.read_outcome_data_to_sentences(args.data+'train.txt')
+        evaluate(data=eval_data, labels=eval_data_labels, train_args=train_args, model=eval_model, tokenizer=tokenizer)
 
 if __name__ == '__main__':
     par = ArgumentParser()
     par.add_argument('--data', default='data/ebm-comet/', help='source of data')
     par.add_argument('--do_train', action='store_true', help='call if you want to fine-tune pretrained model on dataset')
     par.add_argument('--do_eval', action='store_true', help='call if you want to evaluate a fine-tuned pretrained model on validation dataset')
+    par.add_argument('--do_fill', action='store_false', help='Get the model to fill in masked entities in the validation dataset')
     par.add_argument('--output_dir', default='output', help='indicate where you want model and results to be stored')
     par.add_argument('--pretrained_model', default='dmis-lab/biobert-v1.1', help='pre-trained model available via hugging face')
     par.add_argument('--per_device_train_batch_size', default=8, help='training batch size')
