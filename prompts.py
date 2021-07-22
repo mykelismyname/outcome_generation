bfe145add2026b5e17b12a2e1fab36c34347ea10
michealabaho <michealabaho265@gmail.com>
2021-07-22 13:56:18 +0100

    it doesn't matter how many outcomes, just ask, I will answer

--- prompts.py
+++ prompts.py
@@ -30,37 +30,48 @@ def prompts(data, outcome_occurrence, count):
 def prompt(args):
     model = AutoModelForMaskedLM.from_pretrained(args.pretrained_model)
     tokenizer = AutoTokenizer.from_pretrained(args.pretrained_model)
-    prompt = input("Submit a propmt leaving out a blank:\n")
-    try:
-        mask_to_replace = [i.strip() for i in re.findall(r"\s#*\s", prompt)]
-        print('Masked tokens:', mask_to_replace)
-        for m in mask_to_replace:
-            mask = tokenizer.mask_token
-            if len(m) > 1:
-                mask = " ".join(tokenizer.mask_token for i in range(len(m)))
-            masked_text = prompt.replace(m, mask.rstrip(), 1)
-            print('Masked text:-', masked_text)
-            _input_ = tokenizer.encode(masked_text, return_tensors="pt")
-            mask_token_index = torch.where(_input_ == tokenizer.mask_token_id)[1]
-            logits = model(_input_).logits
-            top_token_predictions, all_predictions = [], []
-            for msk in mask_token_index:
-                msk = torch.unsqueeze(msk, 0)
-                mask_token_logits = logits[0, msk, :]
-                print(torch.topk(mask_token_logits, 5, dim=1))
-                top_tokens = torch.topk(mask_token_logits, 5, dim=1).indices[0].tolist()
-                top_token_predictions.append(top_tokens[0])
-                all_predictions.append(top_tokens)
-            prediction = ' '.join([tokenizer.decode([id]) for id in top_token_predictions])
-            # all_predictions = [list(i) for i in zip(*all_predictions)]
-            for i,p in enumerate(list(zip(*all_predictions))):
-                print('Prediction {}:- {}'.format(i+1, ' '.join([tokenizer.decode([id]) for id in p])))
-            un_masked_text = masked_text.replace(mask, prediction, 1)
-            print(un_masked_text)
-    except Exception as e:
-        exc_type, exc_obj, exc_tb = sys.exc_info()
-        print(exc_type, exc_obj, exc_tb.tb_lineno)
-        raise ValueError('Something is wrong {}'.format(e))
+    while True:
+        prompt = input("Submit a propmt leaving out a blank:\n")
+        if prompt == "":
+            break
+        try:
+            prompt_tokens = re.split('\s+', prompt)
+            mask_to_replace = [i for i in prompt_tokens if all(j=='#' for j in i)]
+            print('Masked tokens:', mask_to_replace)
+            for m in mask_to_replace:
+                mask = tokenizer.mask_token
+                if len(m) > 1:
+                    mask = " ".join(tokenizer.mask_token for i in range(len(m)))
+                masked_text = prompt.replace(m, mask.rstrip(), 1)
+                print('Masked text:-', masked_text)
+                _input_ = tokenizer.encode(masked_text, return_tensors="pt")
+                mask_token_index = torch.where(_input_ == tokenizer.mask_token_id)[1]
+                logits = model(_input_).logits
+                top_token_predictions, all_predictions = [], []
+                for msk in mask_token_index:
+                    msk = torch.unsqueeze(msk, 0)
+                    mask_token_logits = logits[0, msk, :]
+                    # print(torch.topk(mask_token_logits, 5, dim=1))
+                    top_tokens = torch.topk(mask_token_logits, 5, dim=1).indices[0].tolist()
+                    top_token_predictions.append(top_tokens[0])
+                    all_predictions.append(top_tokens)
+                # prediction = ' '.join([tokenizer.decode([id]) for id in top_token_predictions])
+
+                for i,p in enumerate(list(zip(*all_predictions))):
+                    # prediction = '\033[1m' + ' '.join([tokenizer.decode([id]) for id in p]) + '\033[0m'
+                    prediction = ' '.join([tokenizer.decode([id]) for id in p])
+                    if i == 0:
+                        un_masked_text = masked_text.replace(mask, prediction, 1)
+                        print(un_masked_text, '\nOther Predictions')
+                    else:
+                        print('Prediction {}:- {}'.format(i+1, prediction))
+
+                prompt = un_masked_text
+        except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            print(exc_type, exc_obj, exc_tb.tb_lineno)
+            raise ValueError('Something is wrong {}'.format(e))
+
 
 
 if __name__ == '__main__':
