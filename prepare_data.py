38e0ed9cf765d8ea236b3b3df85a154e85d2ae80
michealabaho <michealabaho265@gmail.com>
2021-09-08 15:58:04 +0100

    convert train, test and dev txt to json data

--- prepare_data.py
+++ prepare_data.py
@@ -29,17 +29,17 @@ def fetch_data(args, files):
             outcomes_occurrence = {k.split(' [SEP] ')[0].strip().lower():v for k,v in outcomes_occurrence.items()}
             outcomes_occurrence_list = set(list(outcomes_occurrence.values()))
             for text, labels in zip(data_copy, data_labels_copy):
-                found_outcomes = identify_outcome_using_label(seq=text, seq_labels=labels)
+                found_outcomes_labels = identify_outcome_using_label(seq=text, seq_labels=labels)
+                found_outcomes = [i[0] for i in found_outcomes_labels]
                 if not found_outcomes:
                     data.append(text)
                     data_labels.append(labels)
                 else:
                     for o_come in found_outcomes:
-                        if outcomes_occurrence[o_come.lower()] >= np.percentile(list(outcomes_occurrence_list), 75):
+                        if outcomes_occurrence[o_come.lower()] in range(0, int(np.percentile(list(outcomes_occurrence_list), 75))):
                             data.append(text)
                             data_labels.append(labels)
                             break
-
         if file.__contains__('train'):
             train_samples_len = len(data)
         if file.__contains__('dev'):
@@ -151,17 +151,19 @@ def identify_outcome_using_label(seq, seq_labels):
         e = 0
         for m in range(len(x_split)):
             if m == e:
-                outcome = ''
+                outcome, outcome_label = '', ''
                 if y_split[m].strip().startswith('B-'):
                     outcome += x_split[m].strip()
+                    outcome_label += y_split[m].strip()
                     e += 1
                     for w in range(m + 1, len(x_split)):
                         if y_split[w].startswith('I-'):
                             outcome += ' ' + x_split[w].strip()
+                            outcome_label += ' ' + y_split[w].strip()
                             e += 1
                         else:
                             break
-                    sentence_outcomes.append(outcome)
+                    sentence_outcomes.append((outcome, outcome_label))
                 else:
                     e += 1
     return sentence_outcomes
@@ -173,6 +175,18 @@ def create_directory(name=''):
         os.makedirs(_dir)
     return _dir
 
+#
+def check_quartiles(args):
+    outcomes_occurrence = json.load(open(args.data+'/outcome_occurrence.json'))
+    outcomes_occurrence = {k.split(' [SEP] ')[0].strip().lower(): v for k, v in outcomes_occurrence.items()}
+    outcomes_occurrence_list = list(set(list(outcomes_occurrence.values())))
+    print('{}\nUpper percentile:-{}\nMiddle percentile:-{}\nLower percentile:-{}'.format(
+        outcomes_occurrence_list,
+        int(np.percentile(list(outcomes_occurrence_list), 75)),
+        int(np.percentile(list(outcomes_occurrence_list), 50)),
+        int(np.percentile(list(outcomes_occurrence_list), 25))
+    ))
+
 def main(args):
     if args.correct_dataset:
         correct_dataset(args.data)
@@ -183,6 +197,34 @@ def main(args):
         files = [i for i in glob(args.data+'/*.txt') if os.path.basename(i) in ['train.txt', 'dev.txt', 'test.txt']]
         dataset, dataset_labels, train_samples_len, eval_samples_len, test_samples_len = fetch_data(files)
         outcome_frequency(data=dataset, labels=dataset_labels)
+    if args.create_json:
+        data_dir = os.path.abspath(args.data)
+        json_data_dir = os.path.join(data_dir, 'json_files')
+        if not os.path.exists(json_data_dir):
+            os.makedirs(json_data_dir)
+        for file in os.listdir(args.data):
+            # print(file)
+            if file in ['train.txt', 'dev.txt', 'test.txt']:
+                json_file_name = file.split('.')
+                print(os.path.join(json_data_dir, json_file_name[0]+'.json'))
+                with open(data_dir+'/'+file) as f, open(os.path.join(json_data_dir, json_file_name[0]+'.json'),'w') as w:
+                    data, example, text, tags = [], {}, [], []
+                    for i,row in enumerate(f):
+                        if row == '\n':
+                            if text:
+                                example['text'] = text.copy()
+                                example['tags'] = tags.copy()
+                                data.append(example.copy())
+                            text.clear()
+                            tags.clear()
+                            example.clear()
+                        else:
+                            row = row.rstrip()
+                            token, label = row.split(' ')
+                            text.append(str(token.strip()))
+                            tags.append(str(label.strip()))
+                    json.dump(data, w, indent=4)
+
 
 if __name__ == '__main__':
     par = ArgumentParser()
@@ -190,5 +232,9 @@ if __name__ == '__main__':
     par.add_argument('--correct_dataset', action='store_true', help='move trailing punctuation characters to a new line in the dataset')
     par.add_argument('--read_outcome_data', action='store_true', help='reading train, test and validation files independently')
     par.add_argument('--outcome_frequency', action='store_true', help='check most frequent outcome and the context around it')
+    par.add_argument('--create_json', action='store_true',
+                     help='preparing json train, test and dev files')
+
     args = par.parse_args()
     main(args)
+    check_quartiles(args)
