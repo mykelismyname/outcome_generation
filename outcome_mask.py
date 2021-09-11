d2e228f0eea36c132409b80132a66a73fa6eb2dd
michealabaho <michealabaho265@gmail.com>
2021-09-11 22:02:27 +0100

    new custom mask purely using features (labels & tokens) in read in dataset

--- outcome_mask.py
+++ outcome_mask.py
@@ -5,11 +5,12 @@
 import prepare_data
 import re
 from transformers import PreTrainedTokenizerFast, GPT2TokenizerFast
+from datasets import Dataset
 
 '''
     Masking specific tokens of the input dataset
 '''
-def custom_mask(tokenizer, tokenized_input, dataset, dataset_labels, model):
+def oldcustomMask(tokenizer, tokenized_input, dataset, dataset_labels, model):
     v = 0
     print(tokenizer.all_special_tokens, tokenizer.all_special_ids)
     #get a list of unique labels in the dataset
@@ -18,15 +19,19 @@ def custom_mask(tokenizer, tokenized_input, dataset, dataset_labels, model):
 
     inpt_ids = []
     for i, j in tokenized_input.items():
+        print('here', i)
         if i == 'input_ids':
             for n in range(len(j)):
-                seq_ids, indices_to_mask, masked_seq_ids = j[n], [], []
+                seq_ids, indices_to_mask, masked_seq_ids, token_labels = j[n], [], [], dataset_labels[n]
                 tokens = GPT2TokenizerFast.convert_ids_to_tokens(tokenizer, ids=seq_ids) if model.lower() == 'gpt2' else \
                     PreTrainedTokenizerFast.convert_ids_to_tokens(tokenizer, ids=seq_ids)
+
                 # extract outcomes from sequence given they have been labelled
-                seq_outcomes = prepare_data.identify_outcome_using_label(seq=dataset[n], seq_labels=dataset_labels[n])
+                seq_outcomes_labels = prepare_data.identify_outcome_using_label(seq=dataset[n], seq_labels=dataset_labels[n])
+                seq_outcomes = [i[0] for i in seq_outcomes_labels]
                 if len(seq_outcomes) >= 1:
                     try:
+                        p = [i for i in tokenizer.all_special_ids if i != 103]
                         for outcome in seq_outcomes:
                             outcome_tok_ids = [tok for tok in tokenizer.encode(outcome) if
                                                tok not in tokenizer.all_special_ids]
@@ -37,10 +42,53 @@ def custom_mask(tokenizer, tokenized_input, dataset, dataset_labels, model):
                 elif len(seq_outcomes) == 0 and any(k in unique_labels for k in dataset_labels[n].split()):
                     print(dataset[n], dataset_labels[n])
                     raise ValueError('Outcome exists but not identified')
+                # print(len(seq_ids), len(token_labels))
                 inpt_ids.append(seq_ids)
     tokenized_input['input_ids'] = inpt_ids
-
     return tokenized_input
+
+def customMask(tokenized_input, tokenizer, labels_list, mask=False):
+    custom_mask = mask
+    if custom_mask:
+        print('\n\n---------------CUSTOM MASKING-------------------------\n\n')
+        unique_labels_list = labels_list.copy()
+        unique_labels_list.remove('O')
+        input_ids = []
+        for i, j in enumerate(tokenized_input):
+            seq_ids, indices_to_mask, masked_seq_ids, = j['input_ids'], [], []
+            tokens, token_labels = j['tokens'], [labels_list[i] for i in j['ner_tags']]
+            initial_seq_ids_lens = len(seq_ids)
+            assert len(tokens) == len(token_labels), "How comes length of tokens doesn't match langth of ner tags"
+            seq_outcomes_labels = prepare_data.identify_outcome_using_label(seq=' '.join(tokens), seq_labels=' '.join(token_labels))
+            seq_outcomes = [i[0] for i in seq_outcomes_labels]
+            if len(seq_outcomes) >= 1:
+                try:
+                    for outcome in seq_outcomes:
+                        outcome_tok_ids = [tok for tok in tokenizer.encode(outcome) if
+                                           tok not in tokenizer.all_special_ids]
+                        indices_to_mask.append(outcome_tok_ids)
+                        seq_ids = replace_id_with_mask_id(outcome_tok_ids, 103, seq_ids, expand=True)
+                except:
+                    raise ValueError('Outcome exists but not identified')
+            elif len(seq_outcomes) == 0 and any(k in unique_labels_list for k in token_labels):
+                print(tokens, token_labels)
+                raise ValueError('Outcome exists but not identified')
+            final_seq_ids_lens = len(seq_ids)
+            assert initial_seq_ids_lens == final_seq_ids_lens == len(j['ner_labels']) \
+                   == len(j['attention_mask']) == len(j['labels']) == len(j['token_type_ids']), \
+                   "\n-----------------SOMETHING IS WRONG, CHECK OUT THE LENGTH OF THE TENSORS---------------\n"
+            input_ids.append(seq_ids)
+        tokenized_input_ = tokenized_input.remove_columns('input_ids')
+        tokenized_input_ = tokenized_input_.add_column(name='input_ids', column=input_ids)
+        n = 0
+        for i,j in zip(tokenized_input,tokenized_input_):
+            if n < 5:
+                print(i['input_ids'])
+                print(j['input_ids'])
+            n += 1
+        return tokenized_input_
+    else:
+        return tokenized_input
 
 '''
     replace a sequence of id's representing an outcome with a mask id or a replacement value within id's of an input sequence 
