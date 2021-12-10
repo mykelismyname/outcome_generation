903df07cf1f87dafc659de1d8bc85e6d043f6aba
michealabaho <michealabaho265@gmail.com>
2021-12-10 20:40:12 +0000

    introducing randomly initialised embeddings for marker tokens

--- prompt_model.py
+++ prompt_model.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import transformers
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
 class BasicModule(torch.nn.Module):
     def __init__(self):
@@ -18,7 +19,7 @@ class BasicModule(torch.nn.Module):
         return path
 
 class prompt_model(BasicModule):
-    def __init__(self, model, batch_size, hdim, tokenizer, det_criterion, ner_label_ids):
+    def __init__(self, model, batch_size, hdim, tokenizer):
         super(prompt_model, self).__init__()
         self.model = model
         self.tokenizer = tokenizer
@@ -29,15 +30,13 @@ class prompt_model(BasicModule):
         self.bias = nn.Parameter(torch.zeros(len(self.vocab)))
         self.output.bias = self.bias
         self.linear_first = nn.Linear(hdim, 200)
-        self.linear_second = nn.Linear(200, len(ner_label_ids))
-        self.loss_dte = det_criterion
+        self.linear_second = nn.Parameter(torch.rand(200), requires_grad=True)
+        self.marker_tokens = nn.Embedding(len(self.tokenizer.all_special_tokens), hdim)
 
     def prepare_embeddings(self, input_ids, attention_mask, labels, layer):
         batch = transformers.BatchEncoding({'input_ids': input_ids, 'attention_mask': attention_mask, 'labels': labels})
         outputs = self.model(**batch, output_hidden_states=True)
-        mlm_loss = outputs.loss
         mlm_hidden_states = outputs.hidden_states
-        # print(mlm_hidden_states.shape, outputs.logits.shape)
         # last layer
         if layer == 'last':
             emb = mlm_hidden_states[0]
@@ -56,52 +55,76 @@ class prompt_model(BasicModule):
             batch_prompt_types.append(t.strip('[]'))
         return batch_prompt_types
 
-    def comput_loss(self, ner_preds, ner_labels):
-        assert ner_preds.size(0) == ner_labels.size(0)
-        ner_loss = self.loss_dte(ner_preds, ner_labels)
-        return ner_loss
-
     def prompt_conditioning(self, batch_hidden_output, batch_prompt_types):
         batch_size, seq_length, hidden_dimension = batch_hidden_output.size()
         batch_condition_hidden = torch.zeros(batch_size, seq_length, hidden_dimension)
+        
+        def compute_avg_represention(h):
+            attn_in = torch.tanh(self.linear_first(h))
+            attn_out = torch.matmul(attn_in, self.linear_second.unsqueeze(1))
+            attn_dist = torch.softmax(attn_out, dim=0)
+            cond_dist = torch.matmul(attn_dist.unsqueeze(1), h.unsqueeze(1))
+            avg_cond_dist = torch.mean(cond_dist.view(-1, cond_dist.size(-1)), dim=0)
+            # print(h.shape, attn_in.shape, attn_out.shape, attn_dist.shape, cond_dist.shape, avg_cond_dist.shape, batch_hidden_output.shape)
+            return avg_cond_dist
+        
         for i in range(batch_hidden_output.size(0)):
+            # print(1, batch_prompt_types[i])
             if batch_prompt_types[i] == 'prefix':
                 for j in range(batch_hidden_output[i].size(0)):
                     h = batch_hidden_output[i][:j + 1]
-                    attn = self.linear_first(h)
-                    attn = torch.tanh(attn)
-                    attn = self.linear_second(attn)
-                    attn_dist = torch.bmm(attn.unsqueeze(-1), h.unsqueeze(1))
-                    attn_dist = torch.mean(attn_dist, dim=2)
-                    attn_dist = torch.mean(attn_dist, dim=0)
-                    attn_dist = attn_dist.expand(1, attn_dist.size(0))
-                    batch_condition_hidden[i][j] = attn_dist
+                    avg_cond_dist = compute_avg_represention(h)
+                    batch_condition_hidden[i][j] = avg_cond_dist
             elif batch_prompt_types[i] == 'postfix':
                 for j in range(batch_hidden_output[i].size(0)):
                     h = batch_hidden_output[i][j:]
-                    attn = self.linear_first(h)
-                    attn = torch.tanh(attn)
-                    attn = self.linear_second(attn)
-                    attn_dist = torch.bmm(attn.unsqueeze(-1), h.unsqueeze(1))
-                    attn_dist = torch.mean(attn_dist, dim=2)
-                    attn_dist = torch.mean(attn_dist, dim=0)
-                    attn_dist = attn_dist.expand(1, attn_dist.size(0))
-                    batch_condition_hidden[i][j] = attn_dist
+                    avg_cond_dist = compute_avg_represention(h)
+                    batch_condition_hidden[i][j] = avg_cond_dist
             else:
-                h = batch_hidden_output[i]
-                attn = self.linear_first(h)
-                attn = torch.tanh(attn)
-                attn = self.linear_second(attn)
-                attn_dist = torch.bmm(attn.unsqueeze(-1), h.unsqueeze(1))
-                attn_dist = torch.mean(attn_dist, dim=2)
+                for j in range(batch_hidden_output[i].size(0)):
+                    k = [m for m in range(batch_hidden_output[i].size(0)) if m != j]
+                    context_indices = torch.tensor(k).to(device)
+                    h = batch_hidden_output[i].index_select(0, context_indices)
+                    avg_cond_dist = compute_avg_represention(h)
+                    batch_condition_hidden[i][j] = avg_cond_dist
+                    
+        return batch_condition_hidden
+    
+    def add_marker_token_embeddings(self, batch_hidden_output, batch_prompt_types):
+        #'[prefix]', '[postfix]', '[cloze]', '[mixed]', '[null]'
+        batch_size, seq_length, hidden_dimension = batch_hidden_output.size()
+        marker_token_hidden = torch.zeros(batch_size, seq_length, hidden_dimension)
+        for i in range(batch_hidden_output.size(0)):
+            if batch_prompt_types[i] == 'prefix':
+                x = 0
+            if batch_prompt_types[i] == 'postfix':
+                x = 1
+            if batch_prompt_types[i] == 'cloze':
+                x = 2
+            if batch_prompt_types[i] == 'mixed':
+                x = 3
+            if batch_prompt_types[i] == 'null':
+                x = 4
+            h = batch_hidden_output[i] + self.marker_tokens[x]
+            # h = torch.cat([self.marker_tokens[x], batch_hidden_output[i]], dim=0)
+            marker_token_hidden[i] = h
+        return marker_token_hidden
 
-    def forward(self, input_ids, attention_mask, labels, ner_labels, layer='last'):
-        hidd = self.prepare_embeddings(input_ids, attention_mask, labels, layer)
+    def forward(self, input_ids, attention_mask, labels, layer='last', add_marker_tokens=False, prompt_conditioning=False):
         batch_prompt_types = self.prompt_type(input_ids)
-        hidd = F.gelu(self.fcl(hidd))
-        hidd = self.norm(hidd)
-        hid_output = self.output(hidd)
+        if add_marker_tokens:
+            input_ids = torch.cat((input_ids[:, :1], input_ids[:, 2:]), axis=1)
+            attention_mask = torch.cat((attention_mask[:, :1], attention_mask[:, 2:]), axis=1)
+            labels = torch.cat((labels[:, :1], labels[:, 2:]), axis=1)
+            hidd = self.prepare_embeddings(input_ids, attention_mask, labels, layer)
+            hidd = self.add_marker_token_embeddings(batch_hidden_output=hidd, batch_prompt_types=batch_prompt_types)
+        else:
+            hidd = self.prepare_embeddings(input_ids, attention_mask, labels, layer)
+        if prompt_conditioning:
+            hidd = self.prompt_conditioning(batch_hidden_output=hidd, batch_prompt_types=batch_prompt_types).to(device)
+        actv_hidd = F.gelu(self.fcl(hidd))
+        norm_hidd = self.norm(actv_hidd)
+        prompt_output = self.output(norm_hidd)
         # pt_output = torch.softmax(hid_output, dim=2)
-        pt_output = hid_output
-        return pt_output
+        return hidd, prompt_output
 
